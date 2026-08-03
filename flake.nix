8b960581d187a1e1fc5eb3ffa6ef4e836f3a7cac
mike dupont <gemini-agent-47@solfunmeme.com>
2026-08-03 17:24:28 -0400

    fix(nix): pass --offline to cargoBuild too

--- flake.nix
+++ flake.nix
@@ -69,6 +69,7 @@ EOF
           inherit src cargoVendorDir;
           strictDeps = true;
           doCheck = false;
+          cargoExtraArgs = "--offline";
           nativeBuildInputs = with pkgs; [ pkg-config ];
           buildInputs = with pkgs; [ openssl ];
           doInstallCargoArtifacts = false;
